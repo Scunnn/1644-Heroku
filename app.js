6afe17c5a930f053f8dd508eb37e6c0676bb8453
Scunnn <Truongpmgch200134@fpt.edu.vn>
2022-09-24 16:04:05 +0700

    ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Bước 1 Khai báo http module 
 const http = require('http');
+const fs = require('fs');
 //bước 2: Khai báo port
 /*
 Note: muốn deploy lên hosting (Heroku)
@@ -8,9 +9,40 @@ thì cần phải set port với process.env.PORT
 const port = process.env.PORT || 3000;
 //Bước 3: Tạo web server, set header + code, thêm nội dung cho web
 const app = http.createServer((req, res) => {
+   
     res.statusCode = 200; // HTTP_OK
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Chào mừng đến với bình nguyên vô tận ! !</h1>");   
+    if(req.url ==="/"){
+        fs.readFile("./index.html",(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.write(data);
+                res.end
+            }
+        });
+    }else if(req.url ==="/greenwich"){
+        fs.readFile("./greenwich.html",(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.write(data);
+                res.end
+            }
+        });
+    }else if(req.url ==="/cloud"){
+        fs.readFile("./cloud.html",(err,data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.write(data);
+            res.end
+        }
+    });
+    }else {
+        res.end("<h1>404: Page not found</h1>")
+    }
+
 });
 //bước 4: chạy web bằng listen port.
 app.listen(port);
